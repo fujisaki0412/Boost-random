d7cd8711e8b8bd935eee05dbd457fa718196579d
atwosan <atwo.atwo.0412@gmail.com>
2020-03-08 22:17:01 +0900

    shuffleを追加

--- boost_random/rd.pyx
+++ boost_random/rd.pyx
@@ -1,11 +1,12 @@
 from libc.stdlib cimport rand
-cpdef choice(list a):
-    return a[rand() % len(a)]
-cpdef sample(list a, int count):
+def choice(list seq):
+    return seq[rand() % len(seq)]
+def sample(list a, int count):
     cdef list result = [None] * count
     cdef int n = len(a)
     sele = set()
     sele_add = sele.add
+    cdef int i,j
     for i in range(count):
         j = rand() % n
         while j in sele:
@@ -13,3 +14,8 @@ cpdef sample(list a, int count):
         sele_add(j)
         result[i] = a[j]
     return result
+def shuffle(list x, random=None):
+    cdef int i
+    for i in reversed(range(1, len(x))):
+        j = rand() % i+1
+        x[i], x[j] = x[j], x[i]
--- boost_random/test2.py
+++ boost_random/test2.py
@@ -4,15 +4,13 @@ import time
 #boost_random
 b = [i for i in range(30000)]
 s = time.time()
-for i in b:
-    rd.choice(b)
+rd.sample(b, 30000)
 e = time.time() - s
 print("boost_random>>>" + str(e))
 
 #random
 s = time.time()
-for i in b:
-    random.choice(b)
+random.sample(b, 30000)
 e = time.time() - s
 print("random>>>"+ str(e))
 
